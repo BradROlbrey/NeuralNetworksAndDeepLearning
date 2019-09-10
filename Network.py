f20c2644f217a8f331496fc0641aabf1138e9af1
BradROlbrey <berdley1@gmail.com>
2019-09-10 11:39:07 -0500

    Network obj constructor

---
+++ Network.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+
+
+class Network:
+
+	# Input parameter
+	#   sizes: list in which each element is the number of neurons in each layer.
+	def __init__(self, sizes):
+		self.num_layers = len(sizes)
+		self.sizes = sizes
+		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+			# There is one bias for each node, except for those in the input layer.
+		self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+			# Except for the input layer...
+			# weights is a list of layers. Each layer has a list of nodes.
+			# Each node has a list of input weights. There is a weight for each of the nodes from the previous layer.
+			# E.g., weights[1] is a list containing the weights connecting the second layer to the third.
+
+		#print(sizes[1:])
+		# for elem in self.biases:
+		# 	print(elem)
+		for elem in self.weights:
+			print(elem)
+
+		# size_test = [4, 5, 6, 7, 3]
+		# for elem in zip(size_test[:-1], size_test[1:]):
+		# 	print(elem)
---
+++ main.py
@@ -0,0 +1,11 @@
+
+from Network import Network
+
+
+def main():
+	print("Hello, World!")
+	network = Network([2, 3, 2])
+
+
+if __name__ == '__main__':
+	main()
