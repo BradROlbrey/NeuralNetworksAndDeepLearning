4b009238165ecada5973ac1166cc4f73d7825dd2
BradROlbrey <berdley1@gmail.com>
2019-09-10 13:56:34 -0500

    Sigmoid function, cleaned up

--- Network.py
+++ Network.py
@@ -17,33 +17,12 @@ class Network:
 			# Each node has a list of input weights. There is a weight for each of the nodes from the previous layer.
 			# E.g., weights[1] is a list containing the weights connecting the second layer to the third.
 
-		#print(sizes[1:])
-		# for elem in self.biases:
-		# 	print(elem)
-		# for elem in self.weights:
-		# 	print(elem)
-
-		# size_test = [4, 5, 6, 7, 3]
-		# for elem in zip(size_test[:-1], size_test[1:]):
-		# 	print(elem)
 
 ### Miscellaneous functions
 def sigmoid(a):
-	z = 3
-	print(np.exp(z))
-
-	z = np.asarray([1, 3, 4])
-	print(-z)
-
-	y = np.exp(z)
-	print(y)
-	print(1 + y)
-	print(1/(1 + y))
-
-
 	# Operations on a numpy array are applied individually to each element.
 	# So this will return a list of elements with the operations below
 	# 	applied individually to each element.
-	# The list is for a whole layer, consists of the activation values for
+	# The list is for a whole layer, consists of the output values for
 	# 	each node.
 	return 1.0/(1.0 + np.exp(-a))
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from Network import Network, sigmoid
 def main():
 	print("Hello, World!")
 	net = Network([2, 3, 2])
-	sigmoid(1)
 
 
 if __name__ == '__main__':
